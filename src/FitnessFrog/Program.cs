26b6a9f217ff8d92d6d60ce5a3c8bb5e4cd359ba
Munira Omar <munniomer@gmail.com>
2018-08-28 08:38:47 -0700

    added a runningTotal variable

--- src/FitnessFrog/Program.cs
+++ src/FitnessFrog/Program.cs
@@ -6,14 +6,17 @@ namespace FitnessFrog
     {
         static void Main(string[] args)
         {
+            var runningTotal = 0;
 
             //prompt user for minutes exercised
             Console.Write("Enter how many minutes you exercised: ");
             var input = Console.ReadLine();
 
             //Add minutes exercised to total
+
             //Display total minutes exercised to the screen
             Console.WriteLine("You have exercised {0} minutes", input);
+
             //Repeat until user quit
         }
     }
