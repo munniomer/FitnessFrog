eb39d9c69d2efb337a91bfb0fdd07467c39aeee9
Munira Omar <munniomer@gmail.com>
2018-08-28 08:12:28 -0700

    Receiving input

--- src/FitnessFrog/Program.cs
+++ src/FitnessFrog/Program.cs
@@ -8,6 +8,9 @@ namespace FitnessFrog
         {
 
             //prompt user for minutes exercised
+            Console.Write("Enter how many minutes you exercised: ");
+            var input = Console.ReadLine();
+
             //Add minutes exercised to total
             //Display total minutes exercised to the screen
             //Repeat until user quit
