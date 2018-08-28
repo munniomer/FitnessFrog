bdf7419b0fca3983c8ce7823e71d22d3816920ff
Munira Omar <munniomer@gmail.com>
2018-08-28 14:51:37 -0700

    added brackets to if and else if to make it uniform.

--- src/FitnessFrog/Program.cs
+++ src/FitnessFrog/Program.cs
@@ -37,13 +37,25 @@ namespace FitnessFrog
                 }
 
                 else if (minutes <= 10)
+                {
                     Console.WriteLine("Better than nothing, am I right?");
+                }
+                   
                 else if (minutes <= 30)
+                {
                     Console.WriteLine("Way to go hot stuff!");
+                }
+                   
                 else if (minutes <= 60)
+                {
                     Console.WriteLine("You must be a ninja warrior in training");
+                }
+                    
                 else
+                {
                     Console.WriteLine("Okay, now you are just showing off!");
+                }
+                    
                     
                 runningTotal += minutes;
 
