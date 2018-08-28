7af139360d500955fc10bc222c489932882cd27f
Munira Omar <munniomer@gmail.com>
2018-08-28 07:53:40 -0700

    Preparation and Planning of the FitnessFrog App

--- src/FitnessFrog/Program.cs
+++ src/FitnessFrog/Program.cs
@@ -6,7 +6,11 @@ namespace FitnessFrog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //prompt user for minutes exercised
+            //Add minutes exercised to total
+            //Display total minutes exercised to the screen
+            //Repeat until user quit
         }
     }
 }
